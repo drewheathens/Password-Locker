7900308448fb44329aaca1941777f6c9d782bb30
drewheathens <mudavadie@gmail.com>
2019-01-28 09:01:50 +0300

    added testAccount and user classes correctly

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import unittest # Importing the unittest module
-from contact import Contact # Importing the contact class
+from account import Account # Importing the contact class
 
-class TestContact(unittest.TestCase):
+class TestAccount(unittest.TestCase):
 
     '''
-    Test class that defines test cases for the contact class behaviours.
+    Test class that defines test cases for the account class behaviours.
 
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
@@ -15,7 +15,7 @@ class TestContact(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_contact = Contact("James","Muriuki","0712345678","james@ms.com") # create contact object
+        self.new_account = account("James","Muriuki","0712345678","jamesmuriuki","james@ms.com","jimmymuriuki") # create contact object
 
 
     def test_init(self):
@@ -23,33 +23,35 @@ class TestContact(unittest.TestCase):
         test_init test case to test if the object is initialized properly
         '''
 
-        self.assertEqual(self.new_contact.first_name,"James")
-        self.assertEqual(self.new_contact.last_name,"Muriuki")
-        self.assertEqual(self.new_contact.phone_number,"0712345678")
-        self.assertEqual(self.new_contact.email,"james@ms.com")
+        self.assertEqual(self.new_account.first_name,"James")
+        self.assertEqual(self.new_account.last_name,"Muriuki")
+        self.assertEqual(self.new_account.phone_number,"0712345678")
+        self.assertEqual(self.new_account.account_name,"jamesmuriuki")
+        self.assertEqual(self.new_account.email,"james@ms.com")
+        self.assertEqual(self.new_account.password, "jimmymuriuki")
 
 
 if __name__ == '__main__':
     unittest.main()
-    def test_save_contact(self):
+    def test_save_account(self):
         '''
-        test_save_contact test case to test if the contact object is saved into the contact list
+        test_save_account test case to test if the contact object is saved into the account list
         '''
         self.new_contact.save_contact() #saving the new contact
-        self.assertEqual(len(contact.contact_list), 1)
+        self.assertEqual(len(account.account_list), 1)
 if __name__== '__main__':
     unittest.main()
     # Items up here...
 
-    def test_save_multiple_contact(self):
+    def test_save_multiple_account(self):
         '''
-        test_save_multiple_contact to check if we can save multiple contact
-        objects to our contact_list
+        test_save_multiple_accounts to check if we can save multiple acounts
+        objects to our account_list
         '''
-        self.new_contact.save_contact()
-        test_contact = Contact("Test","user","0712345678","test@user.com") # new contact
-        test_contact.save_contact()
-        self.assertEqual(len(Contact.contact_list),2)
+        self.new_account.save_account()
+        test_account = Account("Test","user","0712345678","test@user.com") # new account
+        test_account.save_account()
+        self.assertEqual(len(Account.account_list),2)
 
 if __name__ == '__main__':
     unittest.main()
--- user.py
+++ user.py
@@ -1,22 +1,24 @@
-class Contact:
+class user:
     """
-    Class that generates new instances of contacts.
+    Class that generates new instances of user accounts.
     """
-    contact_list = [] # Empty contact list
+    account_list = [] # Empty contact list
       # Init method up here
-    def save_contact(self):
+    def save_account(self):
 
         '''
-        save_contact method saves contact objects into contact_list
+        save_account method saves account objects into account_list
         '''
 
-        Contact.contact_list.append(self)
+        Accountt.account_list.append(self)
 
-    def __init__(self,first_name,last_name,phone_number,email):
+    def __init__(self,first_name,last_name,phone_number,account_name,email,password):
 
       # docstring removed for simplicity
 
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = phone_number
+        self.account_name = account_name
         self.email = email
+        self.password = password
